30243b896f97cdcdad7df8634050fe06fcc01275
makotobot98 <yunshengduan@gmail.com>
2020-06-12 17:27:21 -0700

    restructure

---
+++ GraphWtLongDirectedPath.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution{
+  public static void main(String[] args) {
+    
+  }
+
+  /**
+   * alg: dfs search
+   */
+  public static boolean possibleDAG(int[][] edges) {
+
+  }
+  public void buildGraph(int[][] edges, Map<Integer, Set<Integer>> map, Map<Integer, Integer> degree) {
+
+  }
+}
